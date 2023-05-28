effb8d5b0b2f40686b2b321f7afe285f35e77c10
chris imoni <imonireal@gmail.com>
2023-05-28 06:36:23 +0100

    initial commit

---
+++ src/main/java/com/chrisimoni/workitemprocessor/WorkItemProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.chrisimoni.workitemprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkItemProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkItemProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrisimoni/workitemprocessor/WorkItemProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisimoni.workitemprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkItemProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
